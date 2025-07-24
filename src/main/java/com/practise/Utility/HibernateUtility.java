f39853afddc6d0f519f8797633bb560326f7b712
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-07-24 14:03:27 +0530

    Add Hibernate utility and product entity classes with configuration

---
+++ src/main/java/com/practise/Utility/HibernateUtility.java
@@ -0,0 +1,17 @@
+package com.practise.Utility;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtility {
+
+	private static SessionFactory factory;
+
+	public static SessionFactory getSessionFactory() {
+
+		if (factory == null) {
+			factory = new Configuration().configure("Hibernate.cfg.xml").buildSessionFactory();
+		}
+		return factory;
+	}
+}
--- src/main/java/com/practise/app/App.java
+++ src/main/java/com/practise/app/App.java
@@ -1,5 +1,9 @@
 package com.practise.app;
 
+import org.hibernate.SessionFactory;
+
+import com.practise.Utility.HibernateUtility;
+
 /**
  * Hello world!
  *
@@ -9,6 +13,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+
+        SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
+
     }
 }
